f119cb451cd8bad41f32e34c32893fc5af97dcf6
feigebaobei <18515195415@163.com>
2020-05-19 20:45:56 +0800

    搬了一部分接口到mockChain

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var didRouter = require('./routes/did');
+var nodeRouter = require('./routes/node');
+var claimRouter = require('./routes/claim');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/did', didRouter);
+app.use('/node', nodeRouter);
+app.use('/claim', claimRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/node.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var cors = require('./cors')
+
+// var session = require('express-session');
+// var FileStore = require('session-file-store')(session)
+
+/* GET users listing. */
+router.route('/vcode')
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200)
+  })
+  .get(cors.corsWithOptions, (req, res, next) =>{
+  // .get((req, res, next) =>{
+    // res.send('respond with a resource');
+    // 生产环境使用短信的方式发送。
+    // 判断参数是否有效
+    // 开发环境使用接口的方式发送。
+    // req.params.phone
+    res.status(200).json({
+      result: true,
+      data: {
+        checkCode: '123456'
+      },
+      message: ""
+    })
+  })
+  .post(cors.corsWithOptions, (req, res, next) => {
+    res.send('post')
+  })
+  .put(cors.corsWithOptions, (req, res, next) => {
+    res.send('put')
+  })
+  .delete(cors.corsWithOptions, (req, res, next) => {
+    res.send('delete')
+  })
+
+router.route('/udidList')
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200)
+  })
+  .get(cors.corsWithOptions, (req, res, next) =>{
+    // console.log('req')
+    // req.params.phone
+    // 判断参数是否有效
+    res.status(200).json({
+      result: true,
+      data: {
+        udidList: [
+          {
+            title: 'airplane',
+            udid: 'did:ttm:u012345678a5201b690bf0b79eb715dad9ae7815efe9800998ecf8427e8d74'
+          },
+          {
+
+            title: 'tank',
+            udid: 'did:ttm:u012340987a5201b690bf0b79eb715dad9ae7815efe9800998ecf8427e8d74'
+          }
+        ]
+      },
+      message: ""
+    })
+  })
+  .post(cors.corsWithOptions, (req, res, next) => {
+    res.send('post')
+  })
+  .put(cors.corsWithOptions, (req, res, next) => {
+    res.send('put')
+  })
+  .delete(cors.corsWithOptions, (req, res, next) => {
+    res.send('delete')
+  })
+
+module.exports = router;
